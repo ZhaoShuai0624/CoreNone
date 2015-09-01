66006aed8c5d8b68d13f5d840f0eb157e0eb68b9
中软mini003 <zhongruanmini003@zhongruanmini003deMac-mini.local>
2015-09-01 19:17:46 +0800

    Initial Commit

---
+++ CoreNone/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CoreNone
+//
+//  Created by 中软mini003 on 15/9/1.
+//  Copyright (c) 2015年 中软mini003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CoreNone/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreNone
+//
+//  Created by 中软mini003 on 15/9/1.
+//  Copyright (c) 2015年 中软mini003. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
